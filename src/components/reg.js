1bb6e83b785fe5e34236f50aefd3b82b74caa304
Zigy-down <mrigorgud@mail.ru>
2022-09-27 20:37:50 +0300

    request?

--- src/components/reg.js
+++ src/components/reg.js
@@ -1,18 +1,44 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
 import './reg.scss';
 
-const Reg = () => {
-  return (
-    <div className="wrapper__reg">
-      <form action="" method="post">
-            <input type="text" className="login" placeholder="Login" name="login"/>
-            <input type="text" className="password" placeholder="Password" name="password"/>
-            <input type="text" className="password--repeat" placeholder="Password repeat" name="passwordRepeat"/>
-            <button type="submit">Sign in</button>
-      </form>
-      <Link to="/auth/">Sign up</Link>
-    </div>
-  )
-};
+
+class Reg extends React.Component {
+  componentDidMount() {
+    const url = "http://localhost:3000/reg";
+    fetch(url)
+      .then((res) => res)
+      .then((data) => console.log(data));
+    }
+    render() {
+      return (
+        <div className="wrapper__reg">
+          <form action="" method="post">
+                <input type="text" className="login" placeholder="Login" name="login"/>
+                <input type="text" className="password" placeholder="Password" name="password"/>
+                <input type="text" className="password--repeat" placeholder="Password repeat" name="passwordRepeat"/>
+                <button type="submit">Sign in</button>
+          </form>
+          <Link to="/auth/">Sign up</Link>
+        </div>
+      )
+    }
+  }
 
 export default Reg;
+
+// const Reg = () => {
+//   return (
+//     <div className="wrapper__reg">
+//       <form action="" method="post">
+//             <input type="text" className="login" placeholder="Login" name="login"/>
+//             <input type="text" className="password" placeholder="Password" name="password"/>
+//             <input type="text" className="password--repeat" placeholder="Password repeat" name="passwordRepeat"/>
+//             <button type="submit">Sign in</button>
+//       </form>
+//       <Link to="/auth/">Sign up</Link>
+//     </div>
+//   )
+// };
+
+// export default Reg;
